ab189b463ba49b60145470368e04f1942f2e7cd1
Dwaipayan Ray <dwaipayanray1@gmail.com>
2021-10-19 01:24:23 +0530

    Add pgadmin configuration

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 sh setup-scripts/minikube-install.sh
 sh setup-scripts/kubegres-install.sh
 
-# Apply configs
+# Apply postgres configs
 kubectl apply -f config/postgres-secret.yaml
 kubectl apply -f config/postgres.yaml
+
+# Apply pgadmin configs
+kubectl apply -f config/pgadmin-secret.yaml
+kubectl apply -f config/pgadmin-configmap.yaml
+kubectl apply -f config/pgadmin-service.yaml
+kubectl apply -f config/pgadmin-statefulset.yaml
