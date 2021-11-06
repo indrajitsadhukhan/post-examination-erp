e3c85e12c30e33c73cea476fa48d1dd228cc2240
Dwaipayan Ray <dwaipayanray1@gmail.com>
2021-11-06 12:28:53 +0530

    Add db drop script

---
+++ db/destroy.sql
@@ -0,0 +1,25 @@
+/*
+
+drop table regulations CASCADE;
+drop table grades CASCADE;
+drop table grades_data CASCADE;
+drop table marks CASCADE;
+drop table exam CASCADE;
+drop table course_examiners CASCADE;
+drop table semester_courses CASCADE;
+drop table course_versions CASCADE;
+drop table course CASCADE;
+drop table semester CASCADE;
+drop table programme_versions CASCADE;
+drop table programme CASCADE;
+drop table role_permissions CASCADE;
+drop table permissions CASCADE;
+drop table user_roles CASCADE;
+drop table roles CASCADE;
+drop table user_data CASCADE;
+drop table users CASCADE;
+
+This may lead to MH sir slapping us. Please don't run this without knowing
+what you are doing.
+
+/*
