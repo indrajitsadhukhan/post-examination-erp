feb728ea77fe53a4c4a17bca8c2be5623e986892
indrajitsadhukhan <indrajitsadhukhan1000@gmail.com>
2022-02-28 22:01:22 +0530

    Updated files

--- microservices/course-module/courseController.js
+++ microservices/course-module/courseController.js
@@ -23,20 +23,12 @@ const fs = require('fs')
 const csv = require('fast-csv')
 require('dotenv').config();
 
-// const pool = new Pool({
-//   user: process.env.USER,
-//   host: process.env.HOST,
-//   database: process.env.DATABASE,
-//   password: process.env.PASSWORD,
-//   port: process.env.PORT,
-// })
-
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'college',
-  password: 'indrajit@2000',
-  port: 5432,
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.PORT,
 })
 
 
