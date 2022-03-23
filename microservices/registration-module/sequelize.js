1804da1c2c7f949f87e3c7da996493a9fb84ef34
Dwaipayan Ray <35661311+raydwaipayan@users.noreply.github.com>
2022-03-23 12:14:51 +0530

    Revert "Add registration service (#6)" (#7)
    This reverts commit 56f41791e8cf5b74795212d593037b3aaadc17d6.

--- microservices/registration-module/sequelize.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require("sequelize");
-
-const DB_URL = process.env.DB_URL;
-const DB_NAME = process.env.DB_NAME;
-
-const sequelize = new Sequelize(`${DB_URL}/${DB_NAME}`); // Connect to postgres
-
-module.exports = sequelize;
