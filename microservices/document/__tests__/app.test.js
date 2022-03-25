cfb6bea61dab9a1181c6ac94a199343d552a5236
Aditi Nath <aditi.mili2000@gmail.com>
2022-03-25 16:41:33 +0530

    Add e2e test for /manage/types/:type

--- microservices/document/__tests__/app.test.js
+++ microservices/document/__tests__/app.test.js
@@ -2,16 +2,23 @@ const app = require('../app');
 const request = require('supertest');
 
 describe("End to end tests", () => {
-    it("/", () => {
-        return request(app)
+    it("/", (done) => {
+        request(app)
             .get("/")
             .set("Accept", "text/html")
-            .expect(200, /api version/);
+            .expect(200, /api version/, done);
     });
-    it("/manage/types", () => {
-        return request(app)
+    it("/manage/types", (done) => {
+        request(app)
             .get("/manage/types")
             .set("Accept", "application/json")
-            .expect(200);
+            .expect(200, done);
+    });
+    it("manage/types/add", (done) => {
+        request(app)
+            .post("/manage/types/add")
+            .set("Accept", "application/json")
+            .send({type: "marksheet"})
+            .expect(422, { error: 'Type is already present' }, done);
     });
 });
