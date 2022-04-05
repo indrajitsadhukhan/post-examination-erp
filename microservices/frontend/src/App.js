0bcdafa87d54c30728673818dd9d551200b14784
Dwaipayan Ray <dwaipayanray1@gmail.com>
2022-04-06 01:40:28 +0530

    Add react router support

--- microservices/frontend/src/App.js
+++ microservices/frontend/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
 import Login from './Pages/Login/login';
 
 function App() {
   return (
     <>
-      <Login />
+      <Router>
+        <Routes>
+            <Route path="/" element={<Login />} />
+        </Routes>
+      </Router>
     </>
   );
 }
