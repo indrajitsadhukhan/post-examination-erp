1b8f80f47e9a712e0caa4394d3f320bb8672e90d
Dwaipayan Ray <dwaipayanray1@gmail.com>
2022-04-06 11:53:00 +0530

    Add dashboard draft

--- microservices/frontend/src/App.js
+++ microservices/frontend/src/App.js
@@ -5,6 +5,7 @@ import {
   Route,
 } from 'react-router-dom';
 import Authenticate from './Pages/Authentication/authenticate';
+import Dashboard from './Pages/Dashboard/dashboard';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Authenticate type="login" />} />
         <Route path="/register" element={<Authenticate type="register" />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
