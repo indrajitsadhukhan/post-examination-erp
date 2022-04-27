2f7062bafb0329764b43418fbd5656da2b6d28a3
Riom Sen <510518013.riom@students.iiests.ac.in>
2022-04-28 00:48:42 +0530

    Tried to add dialog box, but much work ado

--- microservices/frontend/src/Pages/Dashboard/Programme/programme.js
+++ microservices/frontend/src/Pages/Dashboard/Programme/programme.js
@@ -2,6 +2,12 @@
 import React from 'react';
 import { Grid } from '@mui/material';
 import PropTypes from 'prop-types';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import Dialog from '@material-ui/core/Dialog';
+import Button from '@material-ui/core/Button';
 import { ProgrammeCard } from '../../../Components/Cards/Cards';
 
 const data = [
@@ -22,12 +28,50 @@ const data = [
   },
 ];
 
+function App() {
+  const [open, setOpen] = React.useState(false);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return (
+    <div>
+      <Button
+        variant="outlined"
+        color="primary"
+        onClick={handleClickOpen}
+      >
+        Open My Custom Dialog
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>
+          Greetings from GeeksforGeeks
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Do you do coding ?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Close
+          </Button>
+          <Button onClick={handleClose} color="primary" autoFocus>
+            Yes
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
 export default function Programme() {
   return (
     <Grid container spacing={2}>
       <Grid item xs={60} sm={6} md={4} key="Buttons">
         <div style={{ display: 'flex', justifyContent: 'start' }}>
-          <button type="submit" onClick={() => alert('A')}> Add Programme </button>
+          <button type="submit" onClick={() => App}> Add Programme </button>
           <button type="submit"> Enroll into a Programme </button>
         </div>
       </Grid>
